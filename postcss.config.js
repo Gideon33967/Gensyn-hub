a3a9b252c39079da429e774edf064d20908f2475
Gideon33967 <109658192+Gideon33967@users.noreply.github.com>
2025-11-23 13:51:55 +0100

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+---
+
+### **FILE 5: .gitignore**
+1. Inside `gensyn-hub` folder, create: `.gitignore`
+2. Paste:
+```
+node_modules
+.next
+.vercel
+*.log
+.DS_Store
+.env*.local

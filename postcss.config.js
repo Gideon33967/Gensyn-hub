50450fdd462aa05f1ed69cdf84f1e52e7a82c94c
Gideon33967 <109658192+Gideon33967@users.noreply.github.com>
2025-11-23 14:02:33 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,17 +4,3 @@ module.exports = {
     autoprefixer: {},
   },
 }
-```
-
----
-
-### **FILE 5: .gitignore**
-1. Inside `gensyn-hub` folder, create: `.gitignore`
-2. Paste:
-```
-node_modules
-.next
-.vercel
-*.log
-.DS_Store
-.env*.local
